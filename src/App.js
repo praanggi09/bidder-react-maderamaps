df77fe5f1bb45d1652354ac36e7c2512eb2aed56
maderamaps <pmaderama@gmail.com>
2022-08-02 17:02:50 +0700

    commit1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./assets/scss/style.scss";
 import LandingPage from "./pages/LandingPage";
+import BrowserPage from "./pages/BrowserPage";
 import Header from "./parts/Header";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/browse-by" element={<BrowserPage />} />
         </Routes>
       </BrowserRouter>
     </div>
